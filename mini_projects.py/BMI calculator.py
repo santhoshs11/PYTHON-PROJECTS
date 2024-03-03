26ee91af3d40c88b48760bb72d8710a0abd9897e
santhoshs11 <160268565+santhoshs11@users.noreply.github.com>
2024-03-03 11:48:28 +0530

    Update BMI calculator.py
    function modified

--- mini_projects.py/BMI calculator.py
+++ mini_projects.py/BMI calculator.py
@@ -1,5 +1,8 @@
-height=float(input("hieght in metre :"))
-weight=int(input("weight in kg :"))
-bmi=weight/height**2
-bmi_value=round(bmi,2)
-print(f"your BMI is : {bmi_value}")
+def bmi():  
+  height=float(input("hieght in metre :"))
+  weight=int(input("weight in kg :"))
+  bmi=weight/height**2
+  bmi_value=round(bmi,2)
+  print(f"your BMI is : {bmi_value}")
+
+bmi()
