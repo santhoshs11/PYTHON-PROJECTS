216d356c87e070d643efa679d2d51f849a770a4b
SANTHOSH <santhoshs11>
2024-03-13 16:54:23 +0530

    rps game

---
+++ mini_projects.py/rock_paper_scissor.py
@@ -0,0 +1,28 @@
+def rps_game():   
+
+    while (True):   
+        import random
+
+        list =["rock","scissor","paper"]
+        list1 =random.randint(0,2)
+        com = list[list1]
+        user = input("choose your choice  : ")
+        choice = user.lower()
+        if choice =="rock" and com == "scissor" :
+            print(f"compuer choice : {com} \n you win")   
+        elif choice =="rock" and com =="paper":
+            print(f"compuer choice : {com} \n you lose")   
+        elif choice =="scissor" and com =="rock":
+            print(f"compuer choice : {com} \n you lose")   
+        elif choice =="scissor" and com == "paper" :
+            print(f"compuer choice : {com} \n you win")   
+        elif choice == "paper" and com == "rock"  :
+            print(f"compuer choice : {com}\n  \n you win") 
+        elif choice == "paper" and com == "scissor":
+            print(f"compuer choice : {com} \n you lose")   
+        elif choice == com:
+            print (f"computer choice {com}\n draw")     
+        else:
+            print("error")
+
+rps_game()            
